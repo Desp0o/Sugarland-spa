4ab794342da1e525d83f582fca9fd492a04729d1
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-05-27 14:01:36 +0400

    making poor API

--- src/components/dataBase.js
+++ src/components/dataBase.js
@@ -19,6 +19,24 @@ export const massageList =[
             paragraph2:'Deep tissue massages are used to break up scar tissue and break down muscle adhesions (the "knots" that we feel in our muscles are muscle adhesions, which are bands of rigid and painful muscle tissue). These knots can inhibit our circulation and cause pain and inflammation.',
             paragraph3:'When the massage begins, your massage therapist usually starts with lighter pressure to warm up your muscles and then works into deeper pressure.',
             paragraph4:'The common techniques used in deep tissue massage therapy include stripping, which is deep pressure that glides along the length of your muscle fibers and friction, which applies pressure across the grain of your muscle to break up adhesions and align tissue fibers.',
+            header2:"The benefits of massage", //small header
+            paragraph5: "Deep tissue massages have benefits for both your physical and mental health. Any massage will have numerous benefits for the receiver, making them an an easy way to relax and treat your medical issues.",
+            list:{
+                listHeader1:"STRESS RELIEF",
+                listParagraph1:'Deep tissue massage is a great stress reliever. Whether you are experiencing stress at home or at work, getting a massage is a great way to unplug and relax for an extended period of time. A massage can help lower cortisol levels and increase levels of oxytocin, which is a hormone that relaxes the body and produces soothing effects.',
+                listHeader2:"REDUCES PAIN",
+                listParagraph2:'There are a number of conditions that deep tissue massage therapy can treat. Chronic pain in the lower back, plantar fasciitis, fibromyalgia, and stiff necks can all be treated with massage therapy. Muscle tension that usually occurs with chronic pain can also be lessened with deep tissue massage, as it can loosen the tight tissue clusters causing pain.',
+                listHeader3:"LOWERS BLOOD PRESSURE AND HEART RATE",
+                listParagraph3:'Deep tissue massage reduces stress and tension, which impacts blood pressure. Massage has positive impacts on systolic, diastolic, and arterial blood pressure. The massage can also increase production of serotonin, which promotes good feelings and happiness.',
+                listHeader4:"BREAKS UP SCAR TISSUE & MAKES MOVEMENT EASIER",
+                listParagraph4:'In areas where people have scar tissue, they often report chronic pain and stiffness. Getting regular deep tissue massages can help break up this scar tissue by improving lymphatic circulation and drainage to improve flexibility and range of motion in the area of the scar tissue. Many people who are recovering from surgery are often encouraged to get deep tissue massages to minimize scar tissue.',
+                listHeader5:"REDUCES ARTHRITIS SYMPTOMS",
+                listParagraph5:'Deep tissue massage therapy can be used to treat many different symptoms of arthritis, like pain, stiffness, limited range of motion in joints, and sleep issues. Moderate pressure can reduce arthritis pain and ease tension, making it easier for those with arthritis to move around. It can also help with sleep issues by alleviating pain that may keep arthritis patients awake at night.',
+                listHeader6:"REHABILITATES INJURED MUSCLES",
+                listParagraph6:'If you have injured muscles, a deep tissue massage can help stretch tight or twisted muscles and can also facilitate the movement of toxins from your muscles. Deep tissue massage therapy is used frequently to treat sports-related injuries. Many athletes are incorporating deep tissue massage into their recovery protocols, as they can help delay the onset of muscle soreness, help with muscle fatigue, and prevent injuries.',
+            },
+            paragraph6:''
+
         },
 
     },
@@ -28,7 +46,38 @@ export const massageList =[
         image:`${swedish}`,
         name:"Swedish massage",
         shortDescription:"Swedish massage therapy is the most well-known and widely practiced type of therapeutic massage—and for good reason. This type of massage focuses on muscle relaxation, targeting superficial muscles (rather than the connective tissues targeted in deep-tissue massage) and increasing blood circulation.",
-        description:"",
+        description:{
+            paragraph1:'Swedish massage therapy is the most well-known and widely practiced type of therapeutic massage—and for good reason. This type of massage focuses on muscle relaxation, targeting superficial muscles (rather than the connective tissues targeted in deep-tissue massage) and increasing blood circulation.',
+            paragraph2:'Wondering if you should request a reservation for a Swedish massage? Explore some of the benefits below to learn why a Swedish massage might be a great choice for you.',
+            paragraph3:'',
+            paragraph4:'',
+            header2:'The benefits of massage',
+            paragraph5: "",
+            list:{
+                listHeader1:"Pain management",
+                listParagraph1:'If you have a condition like arthritis and are suffering from chronic pain as a result, Swedish massage can be an effective method for managing that pain in a natural way. Talk to your massage therapist about your pain points so he or she can target those areas and use a motion to improve local circulation and reduce muscle tension.',
+                listHeader2:"Decreased Stress & Increased Relaxation",
+                listParagraph2:'Many of us live demanding lives, both overscheduled and over-worked. Stress can manifest in the body with increased muscle tension and sometimes pain. Stress can also manifest in the mind with increased worry or anxiety. Swedish Massages help to clear the body and mind of unwanted responses to stress by calming the nervous system and relaxing muscular tension.',
+                listHeader3:"Increases Flexibility & Range of Motion",
+                listParagraph3:'Swedish Massage can elongate the muscles, open and lubricate the joints and decrease swelling, all of which can ease movement and increase flexibility. By incorporating stretching techniques, Swedish style massages can also increase a more fluid range of motion.',
+                listHeader4:"Increases Overall Energy",
+                listParagraph4:'Increased circulation and decreased stress can lead to a general boost of energy. ',
+                listHeader5:"Improve Posture",
+                listParagraph5:'While postural imbalances have many sources, including overworked muscles due to repetitive strain, overcompensating muscles to ease stress on overworked muscles, prolonged sitting, or injury, Swedish Massage helps to ease the muscular holding that underlies postural imbalance.',
+                listHeader6:"Increase Blood Flow",
+                listParagraph6:'Massage therapy can dilate the blood vessels and widens the membrane pores, improving your body’s ability to deliver fresh blood to muscles and organs. Endorphin Release to Improve Mood Research shows increased serotonin and dopamine in massage recipients, improving their mood and feeling of emotional well-being.',
+                listHeader7:"Support Healthy Immunity",
+                listParagraph7:'When it comes to overall health, it is important to take a holistic approach. Increased physical pain can also increase mental stress and an increase in stress from environmental factors can manifest in the muscles. In addition, any stress can lead to an immune deficiency. Because Swedish Massage encourages relaxation and a decrease in body tension, it supports a healthy immune system.',
+                listHeader8:"Improve Sleep",
+                listParagraph8:'Studies show that getting a massage increases serotonin levels in the body, so you can fall asleep faster and stay asleep longer.',
+                listHeader9:"Relieve Headaches",
+                listParagraph9:'Many headaches or migraines are a result of stress and/or poor circulation. Swedish Massage relieves the stress associated with tension headaches and improves circulation.  ',
+                listHeader10:"Endorphin Release",
+                listParagraph10:'Improve Mood Research shows increased serotonin and dopamine in massage recipients, improving their mood and feeling of emotional well-being.',
+            },
+            paragraph6:'The significant benefits of Swedish Massage make it clear that a regular massage regimen is more than a luxury, but indeed an important ingredient in the cultivation of physical and emotional well-being. Maybe you just want a “relaxation” massage. But know, that in your relaxation, you are also taking care of both your body and mind in fundamental yet important ways.'
+
+        },
 
     },
 
@@ -37,7 +86,33 @@ export const massageList =[
         image:`${swedish}`,
         name:"Ashiatsu barefoot massage",
         shortDescription:"Ashiatsu barefoot massage is performed on a standard massage table that additionally has ceiling-mounted bars for the therapist holds on to for balance and control during the treatment. Oil or lotion is used to enhance the client’s experience.",
-        description:"",
+        description:{
+            paragraph1:'Ashiatsu barefoot massage is performed on a standard massage table that additionally has ceiling-mounted bars for the therapist holds on to for balance and control during the treatment. Oil or lotion is used to enhance the client’s experience.',
+            paragraph2:'The therapist will apply pressure from their feet, massaging the muscles using long, gliding strokes that lengthen the muscles as well as slow, deep compressions to loosen any knots. The force of the therapist’s body weight behind each of the strokes allows for a depth of massage that is not possible using the hands alone.',
+            paragraph3:'Ashiatsu barefoot massage is performed on a standard massage table that additionally has ceiling-mounted bars for the therapist holds on to for balance and control during the treatment. Oil or lotion is used to enhance the client’s experience.',
+            paragraph4:'The therapist will apply pressure from their feet, massaging the muscles using long, gliding strokes that lengthen the muscles as well as slow, deep compressions to loosen any knots. The force of the therapist’s body weight behind each of the strokes allows for a depth of massage that is not possible using the hands alone.',
+            header2:'The benefits of massage',
+            paragraph5: "Ashiatsu massage has a number of advantages over the more traditional types, here are just a few of them:",
+            list:{
+                listHeader1:"It reduces chronic pain",
+                listParagraph1:'As a deep tissue compression massage, Ashiatsu massage is incredibly effective in pain reduction. This type of massage goes below the surface layers, manipulating the deeper layers of soft tissue.In addition to this, the pressure boosts circulation, which has the effect of reducing the kind of inflammation that is associated with chronic pain. Furthermore, it allows for the release of connective tissues, which are responsible for much discomfort people typically feel. This makes it possible for realignment to occur. This type of massage is particularly suitable for easing long-term back pain associated with poor posture, muscle strain, or degenerated or bulging discs.',
+                listHeader2:"It improves overall flexibility",
+                listParagraph2:'Assisted by gravity, the therapist’s body weight when using long, deep strokes is effective in both broadening and lengthening the muscle fibers. Doing this in both directions has the effect of loosening up the muscles and so restoring the body’s natural flexibility – especially in athletes. ',
+                listParagraphInner1:'Ashiatsu massage is particularly good for the iliotibial band, a thick band of tissue on the outside part of the leg running from the hip to the knee. This is important for running, and problems here can often be the route cause of pain in this portion of the leg for distance runners.',
+                listParagraphInner2:'Moreover, old injuries and the associated scar tissues and adhesions also respond well to this type of therapy. Ashiatsu massage, on the other hand, can deliver up to three times more pressure than any of these, but without the associated soreness. In fact, Ashiatsu massage actually feels overall much gentler than other kinds, but with the enhanced benefit of going much deeper.',
+                listParagraphInner3:'All this means that you will be able to get the relief you need without first experiencing more pain, for an overall, more relaxing, and stress-free experience.',
+                listHeader3:"The effects last longer",
+                listParagraph3:'Ashiatsu is perfect for those looking for a really deep and relaxing massage, and, as this is a more intense kind of treatment, the benefits of it can be felt for much longer. For this reason, you may need to rest for a while following this treatment. After-effects of this therapy include increased circulation of both blood and lymphatic nodes. This enhances health alongside promoting more general feelings of wellbeing.',
+                listHeader4:"",
+                listParagraph4:'',
+                listHeader5:"",
+                listParagraph5:'',
+                listHeader6:"",
+                listParagraph6:'',
+            },
+            paragraph6:''
+
+        },
 
     },
 
@@ -46,7 +121,34 @@ export const massageList =[
         image:`${thai}`,
         name:"Thai massage",
         shortDescription:"Thai massage is a form of therapeutic touch that differs in many ways from traditional massage. Instead of a massage table, you lie on a mat on the floor while the provider manipulates your body in certain ways to stimulate organs and improve flexibility. ",
-        description:"",
+        description:{
+            paragraph1:'Thai massage is a form of therapeutic touch that differs in many ways from traditional massage. Instead of a massage table, you lie on a mat on the floor while the provider manipulates your body in certain ways to stimulate organs and improve flexibility. ',
+            paragraph2:'Thai massage is part of traditional Thai medicine. Similar to Chinese medicine, it works with the body\'s energetic pathways. ',
+            paragraph3:'Thai massage practitioners move your limbs around to stretch joints and gently massage key areas of the body according to traditional Thai medicinal theory.',
+            paragraph4:'',
+            header2:'The benefits of massage', 
+            paragraph5: "",
+            list:{
+                listHeader1:"",
+                listParagraph1:'Increased range of motion. Thai massage combines compression, acupressure, and passive stretching. These increase the range of motion in your joints and muscles. This can also improve your posture.',
+                listHeader2:"",
+                listParagraph2:'Helps with back pain. Thai massage tends to focus on areas that can contribute to back pain, like the inner thigh and abdomen. However, if you have constant back pain, it’s best to visit a doctor before getting any type of massage.',
+		listParagraphInner1:'',
+                listParagraphInner2:'',
+                listParagraphInner3:'',
+                listParagraphInner4:'',
+                listHeader3:"",
+                listParagraph3:'Reduces headache intensity. One study showed that nine sessions of traditional Thai massage in a 3-week period can reduce painful headaches in people who have chronic tension headaches or migraines. ',
+                listHeader4:"",
+                listParagraph4:'Lowers stress. In another study, researchers showed that Thai massage reduced stress, especially when combined with plenty of rest. ',
+                listHeader5:"",
+                listParagraph5:'Helps stroke patients. A 2012 study suggests that stroke patients who get Thai massage regularly may be better able to recover the ability to do daily activities. They may also have lower pain levels and sleep better.',
+                listHeader6:"",
+                listParagraph6:'',
+            },
+            paragraph6:''
+
+        },
 
     },
 
--- src/pages/massages/massages.js
+++ src/pages/massages/massages.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { massageList } from "../../components/dataBase";
 import Navbar from "../../components/navbar/navbar";
 import "./massages.css"
+import burgerBlack from '../../components/images/burgerMenuBlack.webp'
 
 export default function Massages(){
     const { id } = useParams();
@@ -11,18 +12,49 @@ export default function Massages(){
     
     return(
         <>
-        <Navbar 
-            bg='navbar whiteBG' 
-            navLinkColor='navMenuLinks black'
-            logoColor='logoP black'
+        <Navbar
+                bg='navbar whiteBG'
+                navLinkColor='navMenuLinks black'
+                logoColor='logoP black'
+                burgerWhite={burgerBlack} 
+                burgerBlack={burgerBlack}
+                firstLoad={burgerBlack}
         />
+
         <div className="massageBody">
             <img className="massagebodyCover" src={massage.image} alt="massage body cover"/>
-            <p>{massageList[id].name}</p>
+            <h1>{massageList[id].name}</h1>
             {massage.description.paragraph1 ? <p>{massage.description.paragraph1}</p> : ''}
             {massage.description.paragraph2 ? <p>{massage.description.paragraph2}</p> : ''}
             {massage.description.paragraph3 ? <p>{massage.description.paragraph3}</p> : ''}
             {massage.description.paragraph4 ? <p>{massage.description.paragraph4}</p> : ''}
+
+            {massage.description.header2 ? <h2>{massage.description.header2}</h2> : ''}
+
+            {massage.description.paragraph5 ? <p>{massage.description.paragraph5}</p> : ''}
+
+            {massage.description.list.listHeader1 ? <h4>{massage.description.list.listHeader1}</h4> : ''}
+            {massage.description.list.listParagraph1 ? <p>{massage.description.list.listParagraph1}</p> : ''}
+
+            {massage.description.list.listHeader2 ? <h4>{massage.description.list.listHeader2}</h4> : ''}
+            {massage.description.list.listParagraph2 ? <p>{massage.description.list.listParagraph2}</p> : ''}
+            {massage.description.list.listParagraphInner1? <p>{massage.description.list.listParagraphInner1}</p> : ''}
+            {massage.description.list.listParagraphInner2? <p>{massage.description.list.listParagraphInner2}</p> : ''}
+            {massage.description.list.listParagraphInner3? <p>{massage.description.list.listParagraphInner3}</p> : ''}
+
+            {massage.description.list.listHeader3 ? <h4>{massage.description.list.listHeader3}</h4> : ''}
+            {massage.description.list.listParagraph3 ? <p>{massage.description.list.listParagraph3}</p> : ''}
+
+            {massage.description.list.listHeader4 ? <h4>{massage.description.list.listHeader4}</h4> : ''}
+            {massage.description.list.listParagraph4 ? <p>{massage.description.list.listParagraph4}</p> : ''}
+
+            {massage.description.list.listHeader5 ? <h4>{massage.description.list.listHeader5}</h4> : ''}
+            {massage.description.list.listParagraph5 ? <p>{massage.description.list.listParagraph5}</p> : ''}
+
+            {massage.description.list.listHeader6 ? <h4>{massage.description.list.listHeader6}</h4> : ''}
+            {massage.description.list.listParagraph6 ? <p>{massage.description.list.listParagraph6}</p> : ''}
+
+            {massage.description.paragraph6 ? <p>{massage.description.paragraph6}</p> : ''}
         </div>
         </>
         
