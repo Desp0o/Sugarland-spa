a4c986378620fc7ebe590fa033df90e587b3efa6
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-05-30 14:12:51 +0400

    changed slider text array

--- src/pages/main/mainTxt.js
+++ src/pages/main/mainTxt.js
@@ -1,19 +1,10 @@
-import { Link } from 'react-router-dom'
+
 import './main.css'
 
 export default function MainTxt(){
     return(
         <div className="mainBgTexts">
             
-            <h1 className="SugarlandSpa">Sugarland Spa</h1> 
-            <p className="SugarlandSpa2">Mobile massage therapy </p> 
-            <p className='LuxuryMassage'>Luxury massage at your place</p>
-
-            <Link to='/pages/services/services'>
-                <div className='mainBTN'>
-                    <p>Explore</p>
-                </div>
-            </Link>
         </div>
     )
 }
