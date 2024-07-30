7b1107bbb8b6ce59657adb8deda37eda4467a55e
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-07-30 14:46:35 +0400

    add 404

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,16 @@ import Home from "./pages/main/main";
 import Services from "./pages/services";
 import Massages from "./pages/massages";
 import Footer from "./components/footer/footer";
+import NotFoundPage from "./pages/NotFoundPage";
+
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path="*" element={<Home />} />
-        <Route exact path="/services" element={<Services />} />
-        <Route
-          exact
-          path="/massages/:linkName"
-          element={<Massages />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/massages/:linkName" element={<Massages />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
 
       <Footer />
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center"}}>404 Not Found</div>
+    )
+}
+
+export default NotFoundPage
